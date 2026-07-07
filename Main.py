9b72473f9a15903659baed190d906ca330585ba8
SupaJersey <jerzy.r.danilczuk@gmail.com>
2026-07-07 13:10:04 +0100

    Expanded Vector2d Functionality Redid functions to account for angular motion

--- Main.py
+++ Main.py
@@ -60,6 +60,19 @@ class Vector2d:
     def dot(self,other):
         return self.x * other.x + self.y * other.y
 
+    def __neg__(self):
+        return Vector2d(-self.x, -self.y)
+
+    def cross(self, other):
+        if isinstance(other, Vector2d):
+            return self.x * other.y - self.y * other.x
+        elif isinstance(other, (int, float)):
+            return Vector2d(other * self.y, -other * self.x)
+        else:
+            raise TypeError("Vector Cross Error")
+
+    def rcross(self, scalar):
+        return Vector2d(-scalar * self.y, scalar * self.x)
 
     def __str__(self):
         return f"({self.x}, {self.y})"
@@ -101,29 +114,38 @@ def overlap_check(min1, max1, min2, max2):
     return min(max1,max2) - max(min1,min2)
 
 def check_collision(block1, block2):
+
     axes1 = calculate_axes_from_block(block1)
     axes2 = calculate_axes_from_block(block2)
     axes = axes1 + axes2
 
     min_overlap = float("inf")
-    minimum_translation_vector_to_resolve = None
+    best_axis = None
 
     corners1 = block1.calculate_corners()
     corners2 = block2.calculate_corners()
 
     for axis in axes:
+
         min1, max1 = project_corners(corners1, axis)
         min2, max2 = project_corners(corners2, axis)
 
         overlap = overlap_check(min1, max1, min2, max2)
+
         if overlap <= 0:
             return False, None, None
-        else:
-            if overlap < min_overlap:
-                min_overlap = overlap
-                minimum_translation_vector_to_resolve = axis
 
-    return True, minimum_translation_vector_to_resolve, min_overlap
+        if overlap < min_overlap:
+            min_overlap = overlap
+            best_axis = axis
+
+    # --- FIX: enforce consistent normal direction ---
+    center_delta = block2.position - block1.position
+
+    if center_delta.dot(best_axis) < 0:
+        best_axis = -best_axis
+
+    return True, best_axis, min_overlap
 
 class Collision_Detection:
     pass
@@ -139,58 +161,69 @@ class Drag:
     def __init__(self, fluid_density=1.225):
         self.rho = fluid_density
 
-    def calculate(self, block, drag_coefficient):
+    def calculate(self, block, drag_coefficient=0.47):
         velocity = block.velocity.magnitude()
         if velocity == 0:
             return Vector2d(0,0)
-        area = block.dimensions[1] * block.dimensions[2]
-
+        area = block.dimensions[0] * block.dimensions[1]
         drag_magnitude = 0.5 * self.rho * drag_coefficient * area * velocity**2
-
         drag_direction = block.velocity.normalise() * -1
-
         return drag_direction * drag_magnitude
 
-class Local_Forces:
-    @staticmethod
-    def calculate_friction(object1, object2,contact_normal):
-        mu1 = object1.friction
-        mu2 = object2.friction
-        mu = math.sqrt(mu1 * mu2)
 
-        relative_velocity = object1.velocity - object2.velocity
-        tangent = Vector2d(-contact_normal.y, contact_normal.x).normalise()
+def apply_collision_impulse(obj1, obj2, normal, penetration, restitution=0.1):
+    corners1 = obj1.calculate_corners()
+    corners2 = obj2.calculate_corners()
 
-        velocity_along_tangent = relative_velocity.dot(tangent)
-        if velocity_along_tangent > 0:
-            friction_direction = tangent * -1
-        else:
-            friction_direction = tangent
+    contact_point = (obj1.position + obj2.position) * 0.5
 
-        magnitude_normal_force = max(0, (object1.forces - object2.forces).dot(contact_normal))
+    r1 = contact_point - obj1.position
+    r2 = contact_point - obj2.position
 
-        magnitude_friction = mu * magnitude_normal_force
+    v1 = obj1.velocity + Vector2d.rcross(None, obj1.angular_velocity, r1)
+    v2 = obj2.velocity + Vector2d.rcross(None, obj2.angular_velocity, r2)
 
-        return(friction_direction * magnitude_friction)
+    relative_velocity = v1 - v2
+    vel_along_normal = relative_velocity.dot(normal)
 
+    if vel_along_normal > 0:
+        return
+
+    r1_cross_n = r1.cross(normal)
+    r2_cross_n = r2.cross(normal)
+
+    denom = (1 / obj1.mass) + (1 / obj2.mass) + \
+            (r1_cross_n ** 2 / obj1.inertia) + (r2_cross_n ** 2 / obj2.inertia)
+
+    j = -(1 + restitution) * vel_along_normal
+    j /= denom
+
+    impulse = normal * j
+
+    obj1.velocity = obj1.velocity + impulse / obj1.mass
+    obj2.velocity = obj2.velocity - impulse / obj2.mass
+
+    obj1.angular_velocity = obj1.angular_velocity + r1.cross(impulse) / obj1.inertia
+    obj2.angular_velocity = obj2.angular_velocity - r2.cross(impulse) / obj2.inertia
 
 
 class Block:
-
-
-    def __init__(self, mass, dimensions, position, velocity, forces, angle):
+    def __init__(self, mass, dimensions, position, velocity, forces, angle, angular_velocity=0.0):
         self.mass = mass
         self.dimensions = dimensions
-        self.position = Vector2d(position[0] , position[1])
-        self.velocity = Vector2d(velocity[0], velocity[1])
-        self.forces = Vector2d(forces[0], forces[1])
+        self.position = Vector2d(position.x, position.y)
+        self.velocity = Vector2d(velocity.x, velocity.y)
+        self.forces = Vector2d(forces.x, forces.y)
         self.angle = angle
+        self.angular_velocity = angular_velocity
+        self.torque = 0.0
 
+        self.inertia = (1.0 / 12.0) * self.mass * (self.dimensions[0] ** 2 + self.dimensions[1] ** 2)
         self.past_velocity = []
 
     @property
     def volume(self):
-        return self.dimensions[0] * self.dimensions[1] * self.dimensions[2]
+        return self.dimensions[0] * self.dimensions[1]
 
     @property
     def density(self):
@@ -198,7 +231,13 @@ class Block:
 
     @property
     def acceleration(self):
-        return self.forces/self.mass
+        return self.forces / self.mass
+
+    @property
+    def angular_acceleration(self):
+        if self.inertia == 0:
+            return 0.0
+        return self.torque / self.inertia
 
     @property
     def momentum(self):
@@ -208,23 +247,44 @@ class Block:
         half_width = self.dimensions[0] / 2
         half_height = self.dimensions[1] / 2
 
-        relative_corners = [Vector2d(-half_width, -half_height), Vector2d(half_width, -half_height), Vector2d(half_width, half_height), Vector2d(-half_width, half_height)]
+        relative_corners = [
+            Vector2d(-half_width, -half_height),
+            Vector2d(half_width, -half_height),
+            Vector2d(half_width, half_height),
+            Vector2d(-half_width, half_height)
+        ]
 
         block_angle_radians = math.radians(self.angle)
         cos_angle = math.cos(block_angle_radians)
         sin_angle = math.sin(block_angle_radians)
 
         world_corners = []
-
         for corner in relative_corners:
-            rotated_corner = Vector2d(corner.x * cos_angle - corner.y *sin_angle, corner.x * sin_angle + corner.y * cos_angle)
+            rotated_corner = Vector2d(
+                corner.x * cos_angle - corner.y * sin_angle,
+                corner.x * sin_angle + corner.y * cos_angle
+            )
             world_corners.append(self.position + rotated_corner)
 
         return world_corners
 
+    def update_position(self, frame_rate, external_forces):
+        dt = 1 / frame_rate
+        self.forces = Vector2d(0, 0)
+        self.torque = 0.0
+
+        for force in external_forces:
+            self.forces = self.forces + force.calculate(self)
+
+        self.velocity = self.velocity + self.acceleration * dt
+        self.position = self.position + self.velocity * dt
+
+        self.angular_velocity = self.angular_velocity + self.angular_acceleration * dt
+        self.angle = self.angle + math.degrees(self.angular_velocity * dt)
+
 
     def __str__(self):
-        return f'A Block of mass {self.mass}, dimensions {self.dimensions[0]} by {self.dimensions[1]} by {self.dimensions[2]} located x:{self.position[0]} y:{self.position[1]} moving x:{self.velocity[0]}/s y:{self.velocity[1]}/s'
+        return f'A Block of mass {self.mass}, dimensions {self.dimensions[0]} by {self.dimensions[1]} by {self.dimensions[2]} located x:{self.position.x} y:{self.position.y} moving x:{self.velocity.x}/s y:{self.velocity.y}/s'
 
     def advanced_print(self):
         print("mass:",self.mass)
@@ -239,13 +299,35 @@ class Block:
         print("momentum:",self.momentum)
 
     def update_position(self, frame_rate, external_forces):
-        pass
+        dt = 1 / frame_rate
+        self.forces = Vector2d(0, 0)
+        for force in external_forces:
+            self.forces = self.forces + force.calculate(self)
+        self.velocity = self.velocity + self.acceleration * dt
+        self.position = self.position + self.velocity * dt
 
+def world_step(objects, frame_rate, external_forces):
 
+    dt = 1 / frame_rate
 
-print(force_resolve(1,0, False))
-print(force_resolve(1,45, False))
-print(force_resolve(1,90, False))
-print(force_resolve(1,180, False))
-print(force_resolve(1,270, False))
+    for obj in objects:
+        obj.update_position(frame_rate, external_forces)
+
+    for i in range(len(objects)):
+        for j in range(i + 1, len(objects)):
+
+            obj1 = objects[i]
+            obj2 = objects[j]
+
+            hit, normal, penetration = check_collision(obj1, obj2)
+
+            if not hit:
+                continue
+
+            resolve_penetration(obj1, obj2, normal, penetration)
+
+            apply_collision_impulse(obj1, obj2, normal, restitution=0.1)
+
+            apply_friction_impulse(obj1, obj2, normal)
+
 
